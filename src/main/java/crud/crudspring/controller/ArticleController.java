258386f71049a42c82e7249e24ae19432add0269
Sunghan Shin <crossrunway01@gmail.com>
2023-05-30 19:03:19 +0900

    게시판 조회, 작성 기능 추가

--- src/main/java/crud/crudspring/controller/ArticleController.java
+++ src/main/java/crud/crudspring/controller/ArticleController.java
@@ -1,12 +1,44 @@
 package crud.crudspring.controller;
 
+import crud.crudspring.domain.Article;
 import crud.crudspring.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class ArticleController {
+
     private final ArticleService articleService;
+
+    @GetMapping("/articles/new")
+    public String createForm(){
+        return "articles/createArticleForm";
+    }
+
+    @PostMapping("/articles/new")
+    public String create(ArticleForm form){
+        Article article = new Article();
+        article.setTitle(form.getTitle());
+        article.setContent(form.getContent());
+        article.setWriter(form.getWriter());
+
+        articleService.create(article);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/articles")
+    public String list(Model model){
+        List<Article> articles = articleService.findArticles();
+        model.addAttribute("articles", articles);
+
+        return "articles/articleList";
+    }
 
     @Autowired
     public ArticleController(ArticleService articleService) {
---
+++ src/main/java/crud/crudspring/controller/ArticleForm.java
@@ -0,0 +1,31 @@
+package crud.crudspring.controller;
+
+public class ArticleForm {
+    private String title;
+    private String content;
+    private String writer;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+}
